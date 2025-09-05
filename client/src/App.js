47263648b398a1a701f6c2752d3a0b004fa07a9b
crowrooster971 <wanlishajna@hotmail.com>
2025-09-05 21:15:31 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Collecte de Retours Utilisateur</h1>
+      <p>Bienvenue dans l'application de collecte de retours !!</p>
+    </div>
+  );
+}
+
+export default App;
